da274f1b9ed42bcf4c1d24c114e510fb0192723c
Ashwin krishna <ashwinkrishna7151@gmail.com>
2023-11-03 20:13:38 +0530

    New rest API are added

--- spring-boot-curd/src/main/java/com/learnwithme/curd/controller/PersonController.java
+++ spring-boot-curd/src/main/java/com/learnwithme/curd/controller/PersonController.java
@@ -5,8 +5,13 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.learnwithme.curd.entity.Person;
@@ -18,6 +23,7 @@ import com.learnwithme.curd.service.PersonService;
  * @author Ashwin
  * */
 @RestController
+@RequestMapping("/person")
 public class PersonController {
 
 	@Autowired
@@ -39,8 +45,41 @@ public class PersonController {
 	 * @param id
 	 * @return Person details
 	 * */
-	@GetMapping("/persondetails/{id}")
+	@GetMapping("/{id}")
 	public ResponseEntity<Person> getPersonDetailsById(@PathVariable("id") int id) throws Exception{
 		return ResponseEntity.status(HttpStatus.OK).body(personService.getPersonById(id));
+	}
+	
+	/**
+	 * API for save Person details
+	 * 
+	 * @param Person
+	 * @return saved/Not
+	 * */
+	@PostMapping("/save")
+	public ResponseEntity<String> savePersonDetails(@RequestBody Person person) throws Exception{
+		return ResponseEntity.status(HttpStatus.OK).body(personService.savePerson(person));
+	}
+	
+	/**
+	 * API for Deleting Person by id
+	 * 
+	 * @param id
+	 * @return saved/Not
+	 * */
+	@DeleteMapping("delete/{id}")
+	public ResponseEntity<String> deletePersonById(@PathVariable("id") int id) throws Exception{
+		return ResponseEntity.status(HttpStatus.OK).body(personService.deletePerson(id));
+	}
+	
+	/**
+	 * API for Updating Person details
+	 * 
+	 * @param Person
+	 * @return updated/Not
+	 * */
+	@DeleteMapping("delete/{id}")
+	public ResponseEntity<String> updatePerson(@RequestBody Person person) throws Exception{
+		return ResponseEntity.status(HttpStatus.OK).body(personService.updatePerson(person));
 	}
 }
